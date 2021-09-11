8b1dca57217ce1e0dadcd20857bbaa31436c2eed
andrewwa-cisco <87471918+andrewwa-cisco@users.noreply.github.com>
2021-09-11 11:39:29 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "network" {
   #policy_name = "rtp-iks-cluster-01"
   policy_name = local.clustername
   dns_servers = local.primary_dns
-  ntp_servers = "198.19.255.137"
+  ntp_servers = ["198.19.255.137"]
   timezone    = local.timezone
   domain_name = local.domain_name
   org_name    = local.organization
