37a2b68b1eed1d59b7a38c17ff40b990dc15c826
andrewwa-cisco <87471918+andrewwa-cisco@users.noreply.github.com>
2022-01-07 12:32:16 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "network" {
 #  org_name = local.organization
 #}
 
-module "version_1-19-15-iks.5" {
+module "k8s_version_1-19-15-iks3" {
   source           = "terraform-cisco-modules/iks/intersight//modules/version"
   version = "=2.1.2"
   policyName     = local.k8s_version_name
